79275b900bf879bb33597e8cbbb92555c537c591
Matheus Freitas <msformigteen@live.com>
2025-09-17 18:39:27 -0300

    adicionar o workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+const base = process.env.BASE_PATH || '/';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  base,
   integrations: [react()],
 });
