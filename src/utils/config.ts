10e919bc7caf301a1bfaf3c2f133d32f06374bb6
Matheus Freitas <msformigteen@live.com>
2026-02-10 08:03:29 -0300

    feat: update url source

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 export const getProjectUrl = () => import.meta.env.PUBLIC_BASE_URL || "http://localhost:3000"
 
-export const getSourceUrl = () => import.meta.env.PUBLIC_SOURCE_URL || "https://sbaecojvevbzkqwnlevo.supabase.co/storage/v1/object/public/Personal/ufba.json"
-
+export const getSourceUrl = () =>
+  import.meta.env.PUBLIC_SOURCE_URL ||
+  "https://gist.githubusercontent.com/FormigTeen/725369126b9bf2bdc051e583cc169c6e/raw/76f804913aaaaf56d9828bfa40ce975bc6d2cb7e/ufba.json"
